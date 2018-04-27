b0965937d66ffe1d115435b0935159c06295b24b
kimyonghyun <chunjoo0@naver.com>
2018-04-27 14:47:34 +0900

    add test projects

---
+++ ProductStore.Api.Tests/Controllers/BaseTestController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using System.IO;
+
+namespace ProductStore.Api.Tests.Controllers
+{
+    public class BaseTestController
+    {
+        private const string ApiUrlBase = "api/products";
+
+        public TestServer CreateServer()
+        {
+            var webHostBuilder = WebHost.CreateDefaultBuilder();
+            webHostBuilder.UseContentRoot(Directory.GetCurrentDirectory());
+            webHostBuilder.UseStartup<Startup>();
+
+            return new TestServer(webHostBuilder);
+        }
+
+        public static class Get
+        {
+            public static string AllProducts => $"{ApiUrlBase}";
+            public static string ProductBy(int id) => $"{ApiUrlBase}/{id}";
+        }
+
+        public static class Post
+        {
+            public static string NewProduct => $"{ApiUrlBase}";
+        }
+
+        public static class Put
+        {
+            public static string ProductBy(int id) => $"{ApiUrlBase}/{id}";
+        }
+
+        public static class Delete
+        {
+            public static string ProductBy(int id) => $"{ApiUrlBase}/{id}";
+        }
+    }
+}
---
+++ ProductStore.Api.Tests/Controllers/ProductControllerTests.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using ProductStore.Api.Models;
+using ProductStore.Api.ViewModels;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ProductStore.Api.Tests.Controllers
+{
+    public class ProductControllerTests : BaseTestController
+    {
+        [Fact]
+        public async Task Create_product_and_response_ok_status_code()
+        {
+            using (var server = CreateServer())
+            {
+                var content = new StringContent(BuildProduct(), Encoding.UTF8, "application/json");
+                var response = await server.CreateClient()
+                   .PostAsync(Post.NewProduct, content);
+
+                var responseBody = await response.Content.ReadAsStringAsync();
+                var product = JsonConvert.DeserializeObject<Product>(responseBody);
+
+                Assert.NotNull(product);
+                Assert.Equal("test-product", product.Name);
+            }
+        }
+
+        [Fact]
+        public async Task Get_product_by_id_and_response_ok_status_code()
+        {
+            using (var server = CreateServer())
+            {
+                var response = await server.CreateClient()
+                    .GetAsync(Get.AllProducts);
+
+                var responseBody = await response.Content.ReadAsStringAsync();
+                var productList = JsonConvert.DeserializeObject<ProductListViewModel>(responseBody);
+
+                Assert.NotNull(productList);
+            }
+        }
+
+        string BuildProduct()
+        {
+            var model = new CreateProductModel
+            {
+                Name = "test-product",
+                Price = 5000,
+                Quantity = 5
+            };
+
+            return JsonConvert.SerializeObject(model);
+        }
+    }
+}
